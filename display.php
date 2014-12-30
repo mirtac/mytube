5aaa07b238743ec6492d0e3910e1642c1c4e58a9
tan <david1992309@gmail.com>
2014-12-31 01:18:25 +0800

    refresh code;ajax's get method to get list;

--- display.php
+++ display.php
@@ -22,7 +22,49 @@ var end = 0;
 var db="mysql";
 var obj;
 //db = "mysql";
-				db = "mongo";
+db = "mongo";
+function clearDiv(){
+		if(arguments.length>1){
+				for (var i = 0; i < arguments.length; i++) {
+						$(arguments[i]).html("");
+				}
+
+		}
+		else if(arguments[0]=="all"){
+				$("#video").html("");
+				$("#videoPlay").html("");
+				$("#relationVideo").html("");
+				$("#commentDiv").html("");
+		}
+}
+function httpGetRequest(method,data) {
+		req = false;
+		if(window.XMLHttpRequest) {
+				try { req = new XMLHttpRequest();
+				} catch(e) {
+						req = false; }
+		} else if(window.ActiveXObject) {
+				try { req = new ActiveXObject("Msxml2.XMLHTTP");
+				} catch(e) {
+						try { req = new ActiveXObject("Microsoft.XMLHTTP");
+						} catch(e) { req = false; } 
+				} 
+		}
+		if(req) {
+				if(method=='addToList'){
+						url = 'handle.php?type=addToList';
+						which=data.which;
+						url+="&which="+which+"&vid="+data.id+"&title="+data.title;
+						//req.onreadystatechange = processSearchReqChange;
+				}
+				else if(method=='comment'){
+				}
+	
+				req.open("GET", url, true);
+				req.send(null);
+		}
+		return true;
+}
 function getData(method,id) {
 		//url = 'http://140.123.101.185:5182/~tan/data/youtube/search.php';
 		//url = 'search.php';
@@ -44,8 +86,7 @@ function getData(method,id) {
 						req.onreadystatechange = processSearchReqChange;
 						$("#message").html('searching...');
 						parameter="search="+$("#search").val()+"&order="+$("#order").val()+"&method="+db;
-						$("#videoPlay").html("");
-						$("#relationVideo").html("");
+						clearDiv("all");
 						start = new Date().getTime();
 				}
 				else if(method=='comment'){
@@ -80,17 +121,16 @@ function processSearchReqChange(){
 				end = new Date().getTime();
 				tmp = req.responseText;
 				//$("#video").html(tmp);
-				console.log(tmp);
 				try {
 						obj = JSON.parse(tmp);
 				} 
 				catch (e) {
-						$("#video").html("");
-						//console.log(tmp);
+						clearDiv("#message");
+						console.log(tmp);
 						return false;
 				}
 //				console.log(obj);
-				parseJsonToList();
+				parseJsonToList("total searching time : "+ (end - start) + " ms");
 		}
 }
 function processTestReqChange(){
@@ -103,57 +143,47 @@ function processCommentReqChange(){
 		if(req.readyState==4){
 				commentHtmlCode='';
 				tmp = req.responseText;
-				console.log(tmp);
 				try{
 						obj = JSON.parse(tmp);
 				}catch (e){
 						console.log('fail on processCommentReqChange');
+						console.log(tmp);
 						return false;
 				}
 				for(var i = 0; i < obj.length; i++) {
 						commentHtmlCode+='<div class="comment"><div class="date">'+obj[i].time; 
 						commentHtmlCode+='</div><div class="name">'+obj[i].name+'</div>';
-						commentHtmlCode+='<div class="content">'+obj[i].content+'</div>';
+						commentHtmlCode+='<div class="content">'+obj[i].content+'</div></div>';
 
 				}
-				$("#commentList").html(commentHtmlCode);
+				$("#commentDiv").html(commentHtmlCode);
 				console.log(commentHtmlCode);
 		}
+		else{
+				console.log("["+req.readyState+"]");
+				tmp = req.responseText;
+				console.log(tmp);
+		}
 }
-function parseJsonToList(){
-
-		//TODO if method == mongo  ->    obj.published need to 
-		/*
-		//	getDate()	getFullYear()	getMonth()	;
-		   var date = new Date(unix_timestamp*1000);
-		// hours part from the timestamp
-		var hours = date.getHours();
-		// minutes part from the timestamp
-		var minutes = "0" + date.getMinutes();
-		// seconds part from the timestamp
-		var seconds = "0" + date.getSeconds();
-
-		// will display time in 10:30:23 format
-		var formattedTime = hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
-		*/
-
+function parseJsonToList(message){
 
 		var innerstring='';
-		//innerstring = "total searching time : "+ (end - start) + " ms <br/>";
-		$("#message").html("total searching time : "+ (end - start) + " ms");
+		$("#message").html(message);
 		for(var i = 0; i < obj.length; i++) {
 				/*date transfer*/
-				time = new Date(obj[i].published.sec * 1000000);
+				time = new Date(obj[i].published.sec * 1000000);//should *1000,because wrong in put data to db
 				obj[i].published=time.getFullYear()+"/"+time.getMonth()+"/"+time.getDate();
 				//obj[i].published=time;
 				
 				/**/
-				innerstring+='<div class="videoList" id="'+obj[i].id+'" onClick="playvideo(this)"><img id="'
-				innerstring+=obj[i].id+'"src="http://i.ytimg.com/vi/'+obj[i].id;
-				innerstring+='/mqdefault.jpg" onerror="imgError(this)"/><div class="info"><div class="title">';
-				innerstring+=obj[i].title+'</div><div><span class="published">'+obj[i].published;
-				innerstring+='</span><br/><span class="author">'+obj[i].author+'</span></div><span class="duration">';
-				innerstring+=obj[i].duration+'</span><div class="viewCount">'+obj[i].viewCount
+				innerstring+='<div class="videoList" id="'+obj[i].id+'" onClick="playvideo(this)">';
+				innerstring+='<img id="'+obj[i].id+'"src="http://i.ytimg.com/vi/'+obj[i].id;
+				innerstring+='/mqdefault.jpg" onerror="imgError(this)"/>';
+				innerstring+='<div class="info">';
+				innerstring+='<div class="title">'+obj[i].title+'</div>';
+				innerstring+='<div class="published">'+obj[i].published;
+				innerstring+='</div><div class="author">'+obj[i].author+'</div><div class="duration">';
+				innerstring+=obj[i].duration+'</div><div class="viewCount">'+obj[i].viewCount
 				innerstring+='</div></div>';
 				innerstring+='<span class="jsondata">'+JSON.stringify(obj[i])+'</span></div>';
 		}
@@ -183,9 +213,12 @@ function playvideo(video){
 		/*comment post form*/
 		htmlcode+='<input type="text" id="comment" name="comment" placeholder="comment"></input>';
 		htmlcode+='<input type="button" onClick="getData(\'comment\',\''+videoJson.id+'\')" value="comment"/>';
-		if($("#relationVideo").html() == "" )$("#relationVideo").html($("#video").html());
+		if($("#relationVideo").html() == "" )$("#relationVideo").html($("#video").html());//TODO relationvideo list
 		$("#video").html('');
 		$("#videoPlay").html(htmlcode);
+			
+		videoJson.which = "history";
+		httpGetRequest("addToList",videoJson);
 		getData('comment',videoJson.id);
 		return;
 }
@@ -230,9 +263,10 @@ function setUserInfo(){
 	<div class="video" id="video"><!-- use js to asign video  -->
 	</div>
 	<div class="videoPlay" id="videoPlay">
+	<!-- for videoPlayer--!>
 	</div>
-	<div class="relationVideo" id="relationVideo"></div>
-	<div class="commentList" id="commentList">
+	<div class="relationVideo" id="relationVideo"><!-- for relation list   --!></div>
+	<div class="commentDiv" id="commentDiv"><!-- show comment under videoPlay--!>
 	</div>
 </div>
 <script>
